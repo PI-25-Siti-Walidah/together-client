8c87a10637813f5c0c50fb89eb6766fdccf57ea0
NadiaDNA <nadiadininurazizah@gmail.com>
2025-10-21 23:57:17 +0700

    add dampak section for beranda page

--- src/app/beranda/media.jsx
+++ src/app/beranda/media.jsx
@@ -13,42 +13,37 @@ export default function Media(){
     ]
     
     return(
-        <section className="bg-white px-6 py-14">
-      <div className="text-center mb-6">
-        <h2 className="text-2xl text-[#6A5ACD] font-bold mb-10">Media Partners</h2>
-      </div>
-
-      <div className="overflow-hidden w-full">
-        <div
-          className="flex gap-12 whitespace-nowrap"
-          style={{
-            animation: "scroll 30s linear infinite",
-          }}
-        >
-          {[...mediaPartners, ...mediaPartners].map((img, i) => (
-            <div key={i} className="relative h-20 w-40 flex-shrink-0">
-            <Image
-              src={img}
-              alt={`Partner ${i + 1}`}
-              className="object-contain"
-              fill
-            />
+        <section className="py-12">
+            <div className="text-center mb-6">
+                <div className="text-3xl text-purple-600 font-bold mb-5">Media Partners</div>
             </div>
-          ))}
-        </div>
 
-        {/* Inline keyframes */}
-        <style jsx>{`
-          @keyframes scroll {
-            0% {
-              transform: translateX(0);
-            }
-            100% {
-              transform: translateX(-50%);
-            }
-          }
-        `}</style>
-      </div>
+            <div className="overflow-hidden w-full">
+                <div className="flex gap-12 whitespace-nowrap"
+                style={{animation: "scroll 30s linear infinite"}}>
+                    {[...mediaPartners, ...mediaPartners].map((img, i) => (
+                    <div key={i} className="relative h-20 w-40 flex-shrink-0">
+                        <Image
+                        src={img}
+                        alt={`Partner ${i + 1}`}
+                        className="object-contain"
+                        fill />
+                    </div>
+                ))}
+                </div>
+                
+                {/* Inline keyframes */}
+                <style jsx>{`
+                    @keyframes scroll {
+                        0% {
+                        transform: translateX(0);
+                        }
+                        100% {
+                        transform: translateX(-50%);
+                        }
+                    }
+                `}</style>
+            </div>
         </section>
     );
 }
