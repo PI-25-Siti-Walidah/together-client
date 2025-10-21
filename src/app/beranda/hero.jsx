39cef38b2350600cfa5e1b4040103d474eca99f2
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-21 15:02:47 +0700

    add hero section

--- src/app/beranda/hero.jsx
+++ src/app/beranda/hero.jsx
@@ -1,7 +1,32 @@
-export default function Hero(){
-    return(
-        <section>
-            
-        </section>
-    );
+export default function Hero() {
+  return (
+    <section>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url('/beranda/hero-section.jpg')",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center flex flex-col">
+          <div className="px-5 py-2 mb-4 backdrop-blur-md rounded-2xl bg-white/10 text-white font-semibold">
+            2.5 Juta Janda telah Terbantu
+          </div>
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold text-white">
+              We Get Her the <br />
+              <span className="text-[#FCC0C5]">Support She Deserves</span>
+            </h1>
+            <p className="mb-5">
+              ToGetHer hadir untuk menyalurkan bantuan sosial, pendidikan, dan
+              kesehatan bagi perempuan yang membutuhkan
+            </p>
+            <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn rounded-sm bg-[#6D123F] text-white border-none">
+              Cari Bantuan
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
