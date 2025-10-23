8574a04973e75c6c00b4b92ba19c9abed76f80df
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-23 21:11:14 +0700

    menambahkan section daftar

--- src/app/detail/[id]/component/daftar.jsx
+++ src/app/detail/[id]/component/daftar.jsx
@@ -1,7 +1,17 @@
-export default function Daftar(){
-    return(
-        <section>
-            
-        </section>
-    )
+export default function Daftar() {
+  return (
+    <section className="mt-3">
+      <div className="card w-[560px] bg-base-100 card-md shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title text-[#6D123F] font-bold">Dukungan Ini Hadir Untukmu</h2>
+          <p className="opacity-80">
+            Daftarkan dirimu sekarang dan akses bantuan dengan mudah.
+          </p>
+          <div className="justify-end card-actions">
+            <button className="btn w-full bg-[#6D123F] rounded-sm border-none text-white">Daftar Sekarang</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
