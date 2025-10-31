96d2bf03573e976812e19e3ef702d7134e3bd20f
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-10-31 14:21:52 +0700

    menambahkan state akun saya

--- src/app/user/[id]/component/akun.jsx
+++ src/app/user/[id]/component/akun.jsx
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+
 export default function Akun() {
+const [AkunSaya, setAkunSaya] = useState({
+  username:'',
+  nama:'',
+  no_telp:'',
+  alamat:'',
+})
+
+useEffect(()=>{
+  const dataUser = {
+  username:'sitindaah11',
+  nama:'Siti Indah',
+  no_telp:'081234567890',
+  alamat:'Sulawesi Selatan',
+}
+
+  setAkunSaya(dataUser)
+},[])
+
+const handleChange = (e) => {
+    const { name, value } = e.target; 
+    setAkunSaya(prevState => ({
+      ...prevState,
+      [name]: value, 
+    }));
+  };
+
   return (
     <section>
       <div className="card bg-[#FFF9F7] card-md shadow-sm">
@@ -10,22 +38,30 @@ export default function Akun() {
             <input
               type="text"
               className="input w-full"
+              value={AkunSaya.username}
+              onChange={handleChange}
             />
 
             <label className="label">Nama Lengkap</label>
             <input
               type="text"
               className="input w-full"
+              value={AkunSaya.nama}
+              onChange={handleChange}
             />
 
             <label className="label">Nomer WhatApp</label>
             <input type="text" 
             className="input w-full" 
+            value={AkunSaya.no_telp}
+              onChange={handleChange}
             />
 
             <label className="label">Alamat</label>
             <input type="text" 
             className="input w-full" 
+            value={AkunSaya.alamat}
+            onChange={handleChange}
            />
 
           </fieldset>
