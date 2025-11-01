80b390f4fdb97ac2a239226ba69dbfe955d86b4b
NadiaDNA <nadiadininurazizah@gmail.com>
2025-11-01 10:35:16 +0700

    add router push for detail riwayat pengajuan user

---
+++ src/app/riwayat/detail/[id]/component/pengajuan.jsx
@@ -0,0 +1,7 @@
+export default function Pengajuan (){
+    return(
+        <section>
+            
+        </section>
+    )
+}
---
+++ src/app/riwayat/detail/[id]/page.js
@@ -0,0 +1,15 @@
+'use client'
+import Bantuan from "./component/bantuan";
+import Pengajuan from "./component/pengajuan";
+import Button from "./component/button"
+
+
+export default function Driwayat() {
+  return (
+   <main>
+    <Bantuan />
+    <Pengajuan />
+    <Button />
+   </main>
+  )
+}
