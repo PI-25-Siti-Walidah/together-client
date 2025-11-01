f59234d8d74a130fb8a491ca065c8495c77cae48
NadiaDNA <nadiadininurazizah@gmail.com>
2025-11-02 01:05:10 +0700

    make button component on detail riwayat page

--- src/app/riwayat/detail/[id]/component/button.jsx
+++ src/app/riwayat/detail/[id]/component/button.jsx
@@ -1,7 +1,29 @@
+import { Upload, MessageSquare } from "lucide-react";
 export default function Button(){
+    const status = "Diterima";
+
+    const renderButton = () => {
+        if (status === "Diterima") {
+            return (
+                <button className="btn w-full md:w-150 bg-[#6D123F] rounded-xl text-white border-none hover:bg-pink-600">
+                    <Upload size={18} />
+                    <span>Kirim Bukti Penerimaan</span>
+                </button>
+            );
+        }
+        if (status === "Selesai") {
+            return (
+                <button className="btn w-full md:w-150 bg-[#6D123F] rounded-xl text-white border-none hover:bg-pink-600">
+                    <MessageSquare size={18} />
+                    <span>Kirim Testimoni</span>
+                </button>
+            );
+        }
+    return null;
+};
     return(
-        <section>
-            
+        <section className="mt-4 flex justify-center">
+            {renderButton()}
         </section>
     )
 }
