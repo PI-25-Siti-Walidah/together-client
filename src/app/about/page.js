b3fe8c3b5d7784233598540584ae2e64e1ef10ed
Abil Zuhrotin <abilzuhrotins@gmail.com>
2025-11-04 00:29:20 +0700

    install chroma grid dari react bites, menambahkan foto dan card tim

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,39 @@
+import ChromaGrid from "@/components/ChromaGrid";
 import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
 
 export default function About() {
+const items = [
+  {
+    image: "/tim/nadin.png",
+    title: "Nadia Dini",
+    subtitle: "Frontend Developer",
+    handle: "@Nadin",
+    borderColor: '#06B6D4',
+    gradient: 'linear-gradient(135deg,#06B6D4,#000)',
+    url: "https://www.linkedin.com/in/nadia-dini-nur-azizah-5384851b8/"
+  },
+  {
+    image: "/tim/osyi.png",
+    title: "Fahma Rosyidah",
+    subtitle: "Backend Developer",
+    handle: "@Osyi",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://www.linkedin.com/in/fahma-rosyidah/"
+  },
+  {
+    image: "/tim/abil.png",
+    title: "Abil Zuhrotin",
+    subtitle: "Frontend Developer",
+    handle: "@Abil",
+    borderColor: '#8B5CF6',
+    gradient: 'linear-gradient(225deg,#8B5CF6,#000)',
+    url: "https://www.linkedin.com/in/abil-zuhrotin-sobabah/"
+  }
+];
+
+
   return (
     <div className="h-full pt-4 lg:pt-32 bg-[#FFF9F7]">
       <h1 className="text-center font-bold text-2xl">Tentang Kami</h1>
@@ -31,14 +63,20 @@ export default function About() {
         </div>
       </div>
       <div className="flex flex-col py-12 px-16 mx-16 my-6 card  bg-base-100 shadow-sm">
-        <h2 className="text-center font-bold text-xl">Tim 
+        <h2 className="text-center font-bold text-xl mb-3">Tim 
               <span className="text-white">_</span>
               <span className="text-[#313131]">To</span>
               <span className="text-[#6D123F]">Get</span>
               <span className="text-[#313131]">Her</span>
         </h2>
         <div>
-
+          <ChromaGrid 
+            items={items}
+            radius={300}
+            damping={0.45}
+            fadeOut={0.6}
+            ease="power3.out"
+          />
         </div>
       </div>
       <Navbar />
